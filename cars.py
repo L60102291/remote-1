0e4714bc3ac49474eb7ccc074ce065e448a31fb9
student <60102291@udst.edu.qa>
2024-01-31 10:31:19 +0300

    initial commit

--- cars.py
+++ cars.py
@@ -1,4 +1,10 @@
-def greeting():
-	print("Hello")
-if __name__=="__main__":
-	greeting()
+import random
+
+def select_random_car_brand():
+    car_brands = ["Toyota", "Ford", "Honda", "Chevrolet", "BMW"]
+    random_brand = random.choice(car_brands)
+    return random_brand
+
+if __name__ == "__main__":
+    random_car_brand = select_random_car_brand()
+    print("Randomly selected car brand:", random_car_brand)
